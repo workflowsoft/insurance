ce8534c896b877905b40ef227d76745e60bd083e
Artyom <altruer@gmail.com>
2014-02-08 20:03:22 +0400

    [*] Factor dependencies

--- backend/custom/Action/Validate.php
+++ backend/custom/Action/Validate.php
@@ -1,10 +1,10 @@
 <?php
 
 /**
- * Action Validate 
- * 
+ * Action Validate
+ *
  * Валидация входящих параметров
- * 
+ *
  * @link http://getfrapi.com
  * @author Frapi <frapi@getfrapi.com>
  * @link /validate
@@ -14,10 +14,46 @@ class Action_Validate extends Frapi_Action implements Frapi_Action_Interface
 
     /**
      * Required parameters
-     * 
+     *
      * @var An array of required parameters.
      */
     protected $requiredParams = array();
+
+    private $dependences = array(
+        // kuts
+        16 => array('amortisation',),
+        // kf
+        17 => array('tariff_program_id', 'franchise_type_id', 'franchise_percent_up', 'franchise_percent_down', 'commercial_carting_flag',),
+        // kvs
+        18 => array('driver_age_down', 'driver_age_up', 'driver_exp_down', 'driver_exp_up', 'is_legal_entity',),
+        // kl
+        19 => array('drivers_count_up', 'drivers_count_down', 'driver_age_down', 'is_legal_entity',),
+        // kp
+        20 => array('car_quantity_down', 'car_quantity_up',),
+        // ksd
+        21 => array('tariff_program_id', 'contract_from_day', 'contract_to_day', 'contract_from_month', 'contract_to_month',),
+        // kps
+        22 => array('ts_no_defend_flag', 'ts_satellite_flag', 'ts_have_electronic_alarm',),
+        // kkv
+        23 => array('commission_percent_up', 'commission_percent_down',),
+        // ko
+        24 => array('is_onetime_payment',),
+        // klv
+        25 => array('tariff_program_id', 'regres_limit_factor_id',),
+        // kctoa
+        26 => array('tariff_program_id', 'regres_limit_factor_id', 'tariff_def_damage_type_id',),
+        // vbs
+        27 => array('tariff_program_id', 'payments_without_references_id',),
+        // ksp
+        28 => array(),
+        // ki
+        29 => array(),
+        // kbm
+        30 => array(),
+        // ka
+        31 => array(),
+    );
+
 
     /* all parameters
         'ts_type_id' => self::TYPE_INT,
@@ -96,10 +132,10 @@ class Action_Validate extends Frapi_Action implements Frapi_Action_Interface
 
     /**
      * To Array
-     * 
-     * This method returns the value found in the database 
+     *
+     * This method returns the value found in the database
      * into an associative array.
-     * 
+     *
      * @return array
      */
     public function toArray()
@@ -109,9 +145,9 @@ class Action_Validate extends Frapi_Action implements Frapi_Action_Interface
 
     /**
      * Default Call Method
-     * 
+     *
      * This method is called when no specific request handler has been found
-     * 
+     *
      * @return array
      */
     public function executeAction()
@@ -120,12 +156,11 @@ class Action_Validate extends Frapi_Action implements Frapi_Action_Interface
     }
 
 
-
     /**
      * Get Request Handler
-     * 
+     *
      * This method is called when a request is a GET
-     * 
+     *
      * @return array
      */
     public function executeGet()
@@ -133,9 +168,9 @@ class Action_Validate extends Frapi_Action implements Frapi_Action_Interface
         //Вытщим то, что пришло с фронта
         $parameters_known = array();
         $parameters_to_validate = array();
-        foreach($this->_parameters as $key => $type) {
-            $tmp = $this->getParam( $key, $type);
-            if(empty($tmp)) {
+        foreach ($this->_parameters as $key => $type) {
+            $tmp = $this->getParam($key, $type);
+            if (empty($tmp)) {
                 $parameters_to_validate[] = $key;
             } else {
                 $parameters_known[$key] = $tmp;
@@ -145,24 +180,24 @@ class Action_Validate extends Frapi_Action implements Frapi_Action_Interface
         $validation = array();
         $db = Frapi_Database::getInstance();
         foreach ($parameters_to_validate as $param) {
-            if($param === 'ts_sum') continue;
-			$sql_from = 'SELECT DISTINCT `' . $param . '` FROM `tariff_coefficients`';
-			$sql_where = '';
-			$first = true;
-			foreach ($parameters_known as $key => $param_known) {
+            if ($param === 'ts_sum') continue;
+            $sql_from = 'SELECT DISTINCT `' . $param . '` FROM `tariff_coefficients`';
+            $sql_where = '';
+            $first = true;
+            foreach ($parameters_known as $key => $param_known) {
                 //dirt for sum
-                if($key === 'ts_sum'){
-				    $sql_where .=  ($first ? ' WHERE' : ' AND') . " `ts_sum_up` >= $param_known AND ts_sum_down <= $param_known";
+                if ($key === 'ts_sum') {
+                    $sql_where .= ($first ? ' WHERE' : ' AND') . " `ts_sum_up` >= $param_known AND ts_sum_down <= $param_known";
                 } else {
-                    $sql_where .=  ($first ? ' WHERE' : ' AND') . " `$key` = $param_known";
+                    $sql_where .= ($first ? ' WHERE' : ' AND') . " `$key` = $param_known";
                 }
-				$first = false;
-			}
+                $first = false;
+            }
             $sql = $sql_from . $sql_where;
 
             $sth = $db->query($sql);
-            if(!$sth) {
-                $this->data['crashed_on']  = array(
+            if (!$sth) {
+                $this->data['crashed_on'] = array(
                     'param' => $param,
                     'query' => $sql,
                 );
@@ -174,17 +209,15 @@ class Action_Validate extends Frapi_Action implements Frapi_Action_Interface
             if (!$results) {
                 throw new Frapi_Error('CANT_CALC_BASET');
             }
-            foreach($results as $value) {
+            foreach ($results as $value) {
                 $validation[$param][] = $value[$param];
             }
 
-		}
+        }
         $this->data = $validation;
         return $this->toArray();
 
     }
-
-
 
 
     /**
@@ -234,8 +267,6 @@ class Action_Validate extends Frapi_Action implements Frapi_Action_Interface
     {
         return $this->toArray();
     }
-
-
 
 
 }
