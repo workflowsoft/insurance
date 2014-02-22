1cc35e64c1a04619fd03067595c2a459d9780052
Павел Тропин <pavel.tropin@parcsis.org>
2014-02-22 16:08:16 +0400

    [+] Доработки по страховым программам [*] Фикс запроса по справочникам марок-моделей

--- backend/custom/Action/Programs.php
+++ backend/custom/Action/Programs.php
@@ -93,17 +93,15 @@ class Action_Programs extends Frapi_Action implements Frapi_Action_Interface
         if ($valid instanceof Frapi_Error) {
             throw $valid;
         }
-        //Получить доступные программы страхования изначально
+        /* Получить доступные программы страхования изначально
+         TODO: Пока сделаем дубово. Мы знаем, что ни один из обязательных для рассчета доп. коэфициентов не влияет на список страховых программ
+         TODO: Сделаем еще дубовее, мы знаем, что  все оставшиеся справочники (относящиеся к тарифной программе не влияют друг на друга). В дальнейшем это может быть не так
+         TODO: Еще мы знаем, что корректировка осущетсвляется только в зависимости от программы страхования и на это тоже жестко забъемся
+        */
         $programQry = 'SELECT DISTINCT TP.id, TP.name FROM
                         (
-                          (SELECT TC.tariff_program_id FROM tariff_coefficients TC WHERE %s GROUP BY TC.tariff_program_id) RC
-		                   INNER JOIN
-                              (SELECT AC.tariff_program_id FROM additional_coefficients AC
-				                INNER JOIN coefficients AF ON AF.id = AC.coefficient_id
-				               WHERE AF.is_mandatory = 1 AND %s
-			               GROUP BY AC.tariff_program_id) ACF
-	                       ON RC.tariff_program_id = ACF.tariff_program_id OR ACF.tariff_program_id IS NULL
-                        )
+                          SELECT TC.tariff_program_id FROM tariff_coefficients TC WHERE %s GROUP BY TC.tariff_program_id
+                        ) RC
                         INNER JOIN tariff_program TP ON TP.id = RC.tariff_program_id';
 
         $baseWhereParts = array(
@@ -111,21 +109,11 @@ class Action_Programs extends Frapi_Action implements Frapi_Action_Interface
             sprintf('(ts_sum_down IS NULL OR ts_sum_down<=%F) AND (ts_sum_up IS NULL OR ts_sum_up>=%F)', $this->getParam('ts_sum', self::TYPE_DOUBLE), $this->getParam('ts_sum', self::TYPE_DOUBLE)),
         );
 
-        $additionalWhereParts = array(
-            sprintf('(ts_age = %u OR ts_age IS NULL)', $this->getParam('ts_age', self::TYPE_INT)),
-            sprintf('(ts_antitheft_id = %u OR ts_antitheft_id IS NULL)', $this->getParam('ts_antitheft_id', self::TYPE_INT)),
-            sprintf('(commercial_carting_flag = %u OR commercial_carting_flag IS NULL)', $this->getParam('commercial_carting_flag', self::TYPE_INT)),
-            sprintf('(drivers_count_down IS NULL OR drivers_count_down<=%F) AND (drivers_count_up IS NULL OR drivers_count_up>=%F)', $this->getParam('drivers_count', self::TYPE_INT), $this->getParam('drivers_count', self::TYPE_INT)),
-            sprintf('(driver_age_down IS NULL OR driver_age_down<=%F) AND (driver_age_up IS NULL OR driver_age_up>=%F)', $this->getParam('driver_age', self::TYPE_INT), $this->getParam('driver_age', self::TYPE_INT)),
-            sprintf('(driver_exp_down IS NULL OR driver_exp_down<=%F) AND (driver_exp_up IS NULL OR driver_exp_up>=%F)', $this->getParam('driver_exp', self::TYPE_INT), $this->getParam('driver_exp', self::TYPE_INT)),
-        );
-
         $ts_type_id = $this->getParam('ts_type_id', self::TYPE_INT);
         if (!empty($ts_type_id))
         {
             $part = sprintf('(ts_type_id = %u OR ts_type_id IS NULL)', $this->getParam('ts_type_id', self::TYPE_INT));
             array_push($baseWhereParts, $part);
-            array_push($additionalWhereParts, $part);
         }
 
         $ts_make_id = $this->getParam('ts_make_id', self::TYPE_INT);
@@ -133,7 +121,6 @@ class Action_Programs extends Frapi_Action implements Frapi_Action_Interface
         {
             $part = sprintf('(ts_make_id = %u OR ts_make_id IS NULL)', $this->getParam('ts_make_id', self::TYPE_INT));
             array_push($baseWhereParts, $part);
-            array_push($additionalWhereParts, $part);
         }
 
         $ts_model_id = $this->getParam('ts_model_id', self::TYPE_INT);
@@ -141,7 +128,6 @@ class Action_Programs extends Frapi_Action implements Frapi_Action_Interface
         {
             $part = sprintf('(ts_model_id = %u OR ts_model_id IS NULL)', $this->getParam('ts_model_id', self::TYPE_INT));
             array_push($baseWhereParts, $part);
-            array_push($additionalWhereParts, $part);
         }
 
         $ts_modification_id = $this->getParam('ts_modification_id', self::TYPE_INT);
@@ -149,7 +135,6 @@ class Action_Programs extends Frapi_Action implements Frapi_Action_Interface
         {
             $part = sprintf('(ts_modification_id = %u OR ts_modification_id IS NULL)', $this->getParam('ts_modification_id', self::TYPE_INT));
             array_push($baseWhereParts, $part);
-            array_push($additionalWhereParts, $part);
         }
 
         $ts_group_id = $this->getParam('ts_group_id', self::TYPE_INT);
@@ -157,21 +142,142 @@ class Action_Programs extends Frapi_Action implements Frapi_Action_Interface
         {
             $part = sprintf('(ts_group_id = %u OR ts_group_id IS NULL)', $this->getParam('ts_group_id', self::TYPE_INT));
             array_push($baseWhereParts, $part);
-            array_push($additionalWhereParts, $part);
         }
 
         //Формируем окончательный запрос на получение программ
 
-        $finalProgramQry = sprintf($programQry, join(' AND ',$baseWhereParts), join(' AND ', $additionalWhereParts));
+        $finalProgramQry = sprintf($programQry, join(' AND ',$baseWhereParts));
         $db = Frapi_Database::getInstance();
         $sth = $db->query($finalProgramQry);
         $results = $sth->fetchAll(PDO::FETCH_ASSOC);
+
+        if ($results)
+        {
+            /* Для каждого тарифа нужно определить:
+                     "Покрываемые риски" (risk_id) additional_coefficients, tariff_coefficients
+                     "Сопосб определения размера ущерба" (tariff_def_damage_type_id) additional_coefficients, tariff_coefficients
+                     "Срок действия договора" (contract_day_down, contract_day_up, contract_month_down, contract_month_up, contract_year_down, contract_year_up) additional_coefficients
+                     "Тип возмещения" (regres_limit_factor_id) additional_coefficients
+                     "Выплата без справок" (payments_without_references_id) additional_coefficients
+                     "Тип франшизы" (franchise_type_id) additional_coefficients
+                     "Размер франшизы" (franchise_percent_down, franchise_percent_up)
+                */
+
+            $targetReferences = array(
+                'risk_id' => array(
+                    'name' => 'risks',
+                    'type'=>'simple',
+                    'columns'=> array('risk_id'),
+                    'tables'=>array('additional_coefficients','tariff_coefficients'),
+                 ),
+                'tariff_def_damage_type_id' => array(
+                        'name' => 'tariff_def_damage_type',
+                        'type'=>'simple',
+                        'columns'=> array('tariff_def_damage_type_id'),
+                        'tables'=>array('additional_coefficients','tariff_coefficients'),
+                 ),
+                'contract_day' => array(
+                    'name' => 'contract',
+                    'type'=>'complex_range',
+                    'columns'=> array('contract_day_down', 'contract_day_up'),
+                    'tables'=>array('additional_coefficients'),
+                ),
+                'contract_month' => array(
+                    'name' => 'contract',
+                    'type'=>'complex_range',
+                    'columns'=> array('contract_month_down', 'contract_month_up'),
+                    'tables'=>array('additional_coefficients'),
+                ),
+                'contract_year' => array(
+                    'name' => 'contract',
+                    'type'=>'complex_range',
+                    'columns'=> array('contract_year_down', 'contract_year_up'),
+                    'tables'=>array('additional_coefficients'),
+                ),
+                'regres_limit_factor_id' => array(
+                    'name' => 'regres_limit',
+                    'type'=>'simple',
+                    'columns'=> array('regres_limit_factor_id'),
+                    'tables'=>array('additional_coefficients'),
+                ),
+                'payments_without_references_id' => array(
+                    'name' => 'payments_without_references',
+                    'type'=>'simple',
+                    'columns'=> array('payments_without_references_id'),
+                    'tables'=>array('additional_coefficients'),
+                ),
+                'franchise_type_id' => array(
+                    'name' => 'franchise_type',
+                    'type'=>'simple',
+                    'columns'=> array('franchise_type_id'),
+                    'tables'=>array('additional_coefficients'),
+                ),
+                'franchise_percent' => array(
+                    'name' => 'franchise_percent',
+                    'type'=>'range',
+                    'columns'=> array('franchise_percent_down', 'franchise_percent_up'),
+                    'tables'=>array('additional_coefficients'),
+                ),
+            );
+
+            foreach ($results as &$program)
+            {
+                $references = array();
+                $concrete_params = array('tariff_program_id'=>$program['id']);
+                $concrete_params = array_merge($concrete_params, $this->params);
+                //Получить корректировки для тарифа, если есть корректировка, соотв. справочник уже не нужен.
+                $corrections = Calculation::GetCorrectedParameters($concrete_params);
+                //Осуществляем корректировку параметров
+                foreach ($corrections as $name => $value)
+                {
+                    $concrete_params[$name] =  $value;
+
+                    $referenceDef =  $targetReferences[$name];
+
+                    if ($referenceDef)
+                    {
+                        $referenceName = $referenceDef['name'];
+                        if (!array_key_exists($referenceName, $references))
+                            $references[$referenceName] = array();
+                        $references[$referenceName] =
+                            array_merge($references[$referenceName],
+                                array(
+                                    'request_parameter' => $name,
+                                    'name' => References::getNameByValue($referenceDef, $value),
+                                    'value' => $value,
+                                    'is_default'=> 1
+                                )
+                            );
+                    }
+                }
+                //Корректировка завершена. Осуществляем подбор значений справочников
+                foreach($targetReferences as $param=>$referenceDef)
+                {
+                    //Только если по параметру справочника не проходила корректировка
+                    if (!array_key_exists($param, $corrections))
+                    {
+                        $referenceValues = References::getReferenceByRequestParams($param, $referenceDef, $concrete_params);
+                        if (count($referenceValues))
+                        {
+                            $referenceName = $referenceDef['name'];
+                            if (!array_key_exists($referenceName, $references))
+                                $references[$referenceName] = array();
+                            $references[$referenceName] = array_merge($references[$referenceName], $referenceValues);
+                        }
+                    }
+                }
+
+                $program['references'] = $references;
+            }
+        }
+
         $this->data['Result'] = $results;
+
         /* Рассчет допобуродования отдельно
         additional_sum
         */
 
-        //Получить корректировки для тарифа, если есть корректировка, соотв. справочник уже не нужен.
+
         //Получить диапазоны допустимых значений для справочников каждого из тарифов. Если только null этот справочник для него не нужен
         //Рассчитать суммы для значений по-умолчанию
 
